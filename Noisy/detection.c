312b72de19fdc2570619f1a6e7790aa5ad4f70e5
Amaia <amaia.soubelet@epfl.ch>
2020-05-07 11:26:47 +0200

    Add comments

--- Noisy/detection.c
+++ Noisy/detection.c
@@ -6,16 +6,12 @@
 #include <main.h>
 #include <motors.h>
 #include <sensors/proximity.h>
-#include <sensors/VL53L0X/VL53L0X.h>
 #include <detection.h>
 #include <stdbool.h>
-#include <audio/play_melody.h>
 #include <leds.h>
 
 
 void sensors_init(void) {
-    // TOF sensor
-    //VL53L0X_start();
     // Proximity sensors
     proximity_start();
     calibrate_ir();
