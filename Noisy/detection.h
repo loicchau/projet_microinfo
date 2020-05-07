d486c4a48ec609f787ea0824e45b7e63a8d66cb4
Amaia <amaia.soubelet@epfl.ch>
2020-05-07 11:36:04 +0200

    Comment ir sensor

--- Noisy/detection.h
+++ Noisy/detection.h
@@ -1,14 +1,14 @@
 #ifndef DETECTION_H
 #define DETECTION_H
 
-#define PROX_FRONT_RIGHT_F		0
-#define PROX_FRONT_RIGHT_R		1
-#define PROX_RIGHT				2
-#define PROX_BACK_RIGHT			3
-#define PROX_BACK_LEFT			4
-#define PROX_LEFT				5
-#define PROX_FRONT_LEFT_L		6
-#define PROX_FRONT_LEFT_F		7
+#define PROX_FRONT_RIGHT_F		0 //IR1
+#define PROX_FRONT_RIGHT_R		1 //IR2
+#define PROX_RIGHT				2 //IR3
+#define PROX_BACK_RIGHT			3 //IR4
+#define PROX_BACK_LEFT			4 //IR5
+#define PROX_LEFT				5 //IR6
+#define PROX_FRONT_LEFT_L		6 //IR7
+#define PROX_FRONT_LEFT_F		7 //IR8
 
 #define NB_PROX_SENSOR			8
 
