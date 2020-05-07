0d38857c56da6d4b6d7cb74d3113a3cb64573c70
Loïc Chautems <loic.chautems@hotmail.com>
2020-05-07 10:10:56 +0200

    Clear some fonctions

--- Noisy/audio_processing.c
+++ Noisy/audio_processing.c
@@ -15,13 +15,13 @@
 //2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
 static float micLeft_cmplx_input[2 * FFT_SIZE];
 static float micRight_cmplx_input[2 * FFT_SIZE];
-static float micFront_cmplx_input[2 * FFT_SIZE];
+//static float micFront_cmplx_input[2 * FFT_SIZE];
 static float micBack_cmplx_input[2 * FFT_SIZE];
 
 //Arrays containing the computed magnitude of the complex numbers
 static float micLeft_output[FFT_SIZE];
 static float micRight_output[FFT_SIZE];
-static float micFront_output[FFT_SIZE];
+//static float micFront_output[FFT_SIZE];
 static float micBack_output[FFT_SIZE];
 
 #define MIN_VALUE_THRESHOLD 20000
@@ -39,18 +39,22 @@ static float micBack_output[FFT_SIZE];
 
 
 /*
-*	Simple function used to detect the highest value in a buffer
-*	and to execute a motor command depending on it
+*
 */
 void sound_remote(float* back){
+
 	float prox_values[NB_PROX_SENSOR];
 	float phase_average_left = 0, phase_average_right = 0;
 	float mag_average_left = 0, mag_average_right = 0;
-	//float mag_average_front = 0;
 	volatile int16_t max_norm_index = -1;
 
+	// Fonction qui store les valeurs des capteurs infrarouges dans prox_values
 	obstacle_detection(prox_values);
 
+	/*
+	 * Début de la machine d'état fini
+	 * Detecte d'abord s'il y a un obstacle, sinon suit la source sonore
+	 */
 	if(prox_values[PROX_FRONT_RIGHT_R] > MIN_PROX_THRESHOLD && (prox_values[PROX_RIGHT] < prox_values[PROX_FRONT_RIGHT_R] ||
 																prox_values[PROX_FRONT_RIGHT_R] < prox_values[PROX_FRONT_RIGHT_F])){
 		left_motor_set_speed(-300);
@@ -104,19 +108,21 @@ void sound_remote(float* back){
 		}
 	}
 	
+
 	else{
-		//search for the highest peak
+		// search for the highest peak and compute an average of the magnitude and the phase
 		for(uint16_t i = 2*MIN_FREQ ; i <= 2*MAX_FREQ ; i+=2){
+
 			phase_average_right += atan2(micRight_cmplx_input[i+1],micRight_cmplx_input[i])/(MAX_FREQ-MIN_FREQ);
 			phase_average_left += atan2(micLeft_cmplx_input[i+1],micLeft_cmplx_input[i])/(MAX_FREQ-MIN_FREQ);
 			mag_average_left += micLeft_output[i/2]/(MAX_FREQ-MIN_FREQ);
 			mag_average_right += micRight_output[i/2]/(MAX_FREQ-MIN_FREQ);
-			//mag_average_front += micFront_output[i]/(MAX_FREQ-MIN_FREQ);
+
 			if(back[i/2] > MIN_VALUE_THRESHOLD){
 				max_norm_index = i/2;
 			}
 		}
-		//
+		// Suit la source sonore
 		if(max_norm_index >= FREQ_MOVE_L && max_norm_index <= FREQ_MOVE_H){
 			if(mag_average_left > mag_average_right + MIN_MAG_THRESHOLD_LEFT && phase_average_left > phase_average_right){//||
 				left_motor_set_speed(-300);
@@ -181,14 +187,14 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 		micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
 		micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
 		micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-		micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+		//micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
 
 		nb_samples++;
 
 		micRight_cmplx_input[nb_samples] = 0;
 		micLeft_cmplx_input[nb_samples] = 0;
 		micBack_cmplx_input[nb_samples] = 0;
-		micFront_cmplx_input[nb_samples] = 0;
+		//micFront_cmplx_input[nb_samples] = 0;
 
 		nb_samples++;
 
@@ -207,7 +213,7 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 
 		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
 		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
-		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+		//doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
 		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
 
 
@@ -221,7 +227,7 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 		*/
 		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
 		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+		//arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
 		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
 
 		nb_samples = 0;
--- Noisy/main.c
+++ Noisy/main.c
@@ -20,7 +20,7 @@
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
-
+/*
 static void serial_start(void)
 {
 	static SerialConfig ser_cfg = {
@@ -38,8 +38,8 @@ static void timer12_start(void){
     //timer 12 is a 16 bit timer so we can measure time
     //to about 65ms with a 1Mhz counter
     static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
+        1000000,        // 1MHz timer clock in order to measure uS.
+        NULL,           // Timer callback.
         0,
         0
     };
@@ -47,7 +47,7 @@ static void timer12_start(void){
     gptStart(&GPTD12, &gpt12cfg);
     //let the timer count to max value
     gptStartContinuous(&GPTD12, 0xFFFF);
-}
+}*/
 
 int main(void)
 {
@@ -59,15 +59,15 @@ int main(void)
     messagebus_init( & bus, & bus_lock, & bus_condvar);
 
     //starts the serial communication
-    serial_start();
+    //serial_start();
     //starts the USB communication
-    usb_start();
+    //usb_start();
     //starts timer 12
-    timer12_start();
+    //timer12_start();
     //inits the motors
     motors_init();
 
-    //start the proximity sensors and the TOF sensor processing thread
+    //start the proximity sensors processing thread
     sensors_init();
 
     //starts the microphones processing thread.
